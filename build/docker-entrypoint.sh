b9b1dce49bb2b57c87a309966acacf445cee1992
Yun Seongmin <ilcm96@gmail.com>
2021-07-21 22:01:59 +0900

    Change app to django from golang

---
+++ build/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Collecting static files"
+python3 manage.py collectstatic --noinput
+
+echo "Performing database mirgations"
+python3 manage.py migrate
+
+echo "Starting server"
+gunicorn --bind 0.0.0.0:8000 config.wsgi:application
