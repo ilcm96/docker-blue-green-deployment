4c63955a50a80468d2104eeab72a754abc148959
Yun Seongmin <ilcm96@gmail.com>
2021-07-22 00:01:43 +0900

    Format ouput msg on docker-entrypoint.sh

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -4,9 +4,12 @@ mkdir /app/static && mkdir /app/staticfiles
 
 echo "======Collecting static files======"
 python3 manage.py collectstatic --noinput
+echo
 
 echo "======Performing database mirgations======"
 python3 manage.py migrate
+echo
 
 echo "======Starting server======"
 gunicorn --bind 0.0.0.0:8000 config.wsgi:application
+echo
